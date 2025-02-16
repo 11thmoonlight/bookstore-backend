a6bc0bf01a3422d93ba10eebf9a197b8dd7b2f93
aida <aidasaeedi1998@gmail.com>
2025-02-16 23:18:20 +0330

    change order settings

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -1,3 +1,33 @@
-import { factories } from "@strapi/strapi";
+// import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController("api::order.order");
+// export default factories.createCoreController("api::order.order");
+
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { data } = ctx.request.body;
+
+    // چک کردن اینکه یوزر لاگین کرده باشد
+    if (!ctx.state.user) {
+      return ctx.unauthorized("User not authenticated");
+    }
+
+    // مقداردهی خودکار user
+    const orderData = {
+      ...data,
+      users_permissions_user: ctx.state.user.id,
+    };
+
+    try {
+      const order = await strapi.entityService.create("api::order.order", {
+        data: orderData,
+      });
+
+      return order;
+    } catch (error) {
+      return ctx.internalServerError("Failed to create order", error);
+    }
+  },
+}));
