7d7eabdfd9acd54f072ca62189950398451120ca
aida <aidasaeedi1998@gmail.com>
2025-03-19 01:38:37 +0330

    fix address

--- src/api/payment/controllers/payment.ts
+++ src/api/payment/controllers/payment.ts
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports = {
   async createSession(ctx) {
@@ -11,20 +11,22 @@ module.exports = {
 
       // ایجاد جلسه پرداخت در Stripe با مبلغ کل
       const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [{
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'Order Total',
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: "Order Total",
+              },
+              unit_amount: Math.round(totalPrice * 100), // تبدیل به سنت
             },
-            unit_amount: Math.round(totalPrice * 100), // تبدیل به سنت
+            quantity: 1,
           },
-          quantity: 1,
-        }],
-        mode: 'payment',
-        success_url: `http://localhost:3000/cart/checkout/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `http://localhost:3000/cart/checkout/cancel`,
+        ],
+        mode: "payment",
+        success_url: `http://https://hinode.vercel.app/cart/checkout/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `http://https://hinode.vercel.app/cart/checkout/cancel`,
         metadata: {
           shippingInfo: JSON.stringify(shippingInfo),
         },
@@ -33,7 +35,7 @@ module.exports = {
       return ctx.send({ sessionId: session.id });
     } catch (error) {
       console.error("خطا در ایجاد جلسه پرداخت:", error);
-      return ctx.badRequest('خطا در ایجاد جلسه پرداخت', { error });
+      return ctx.badRequest("خطا در ایجاد جلسه پرداخت", { error });
     }
   },
 };
