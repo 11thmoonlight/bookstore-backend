c5e9ed2b602ac50761fdbe465282a5e2d9b0624c
aida <aidasaeedi1998@gmail.com>
2025-03-28 23:51:58 +0330

    fix middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,28 @@
+// export default [
+//   "strapi::logger",
+//   "strapi::errors",
+//   "strapi::security",
+//   "strapi::cors",
+//   "strapi::poweredBy",
+//   "strapi::query",
+//   "strapi::body",
+//   "strapi::session",
+//   "strapi::favicon",
+//   "strapi::public",
+// ];
+
 export default [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["https://hinode.vercel.app"],
+      headers: ["Content-Type", "Authorization"],
+      credentials: true,
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
